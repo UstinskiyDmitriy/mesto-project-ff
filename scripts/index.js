7122f64dd22cf86c0fe4f286faab90ecc496d86c
Ustinskiy Dmitriy <ustinskiy.dmitriy1997@yandex.ru>
2024-05-01 11:17:32 +0300

    fixed mistakes after first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,32 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
-const placesItem = cardTemplate.querySelector('.places__item')
+const placesItem = cardTemplate.querySelector('.places__item');
 
 // @todo: DOM узлы
-const content = document.querySelector('.content')
-const places = content.querySelector('.places')
-const placesList = places.querySelector('.places__list')
+const content = document.querySelector('.content');
+const places = content.querySelector('.places');
+const placesList = places.querySelector('.places__list');
 
 
 
-// @todo: Функция создания карточки
-function addCard() {
-initialCards.forEach(function(element) {
-  const card = cardTemplate.querySelector('.card').cloneNode(true)
-  const СardImage = card.querySelector('.card__image').src = element.link
-  const cardTitle = card.querySelector('.card__title').textContent = element.name
-  const deleteButton = card.querySelector('.card__delete-button')
+// // @todo: Функция создания карточки
+function createCard(element, deleteCard){
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__image').src = element.link;
+  card.querySelector('.card__title').textContent = element.name;
+  card.querySelector('.card__image').alt = element.name;
+  const deleteButton = card.querySelector('.card__delete-button');
   deleteButton.addEventListener('click',function(){
-   deleteCard(card)
-  })
-  placesList.append(card)
-})
+   deleteCard = removeCard(card)
+  });
+  return card
 }
-
-// @todo: Функция удаления карточки
-function deleteCard(card) {
+// // @todo: Функция удаления карточки
+function removeCard(card) {
   card.remove()
 }
 
-
-// @todo: Вывести карточки на страницу
-addCard()
+// // @todo: Вывести карточки на страницу
+initialCards.forEach(function(element){
+  placesList.append(createCard(element))
+})
